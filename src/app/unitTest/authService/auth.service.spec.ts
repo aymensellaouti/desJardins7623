904c40ddded2ffdf4f56eeb4b0df7e9ac7168725
aymen2782 <aymen.sellaouti@gmail.com>
2023-06-12 08:06:53 +0100

    Ajout de la correction des exercices de cours pour la partie tests unitaires

--- src/app/unitTest/authService/auth.service.spec.ts
+++ src/app/unitTest/authService/auth.service.spec.ts
@@ -1,5 +1,20 @@
-describe("AuthService", () => {
-  it('should work', () => {
-    expect(1).toBe(1);
+import { AuthService } from "./auth.service";
+
+fdescribe("AuthService", () => {
+  let authService: AuthService;
+
+  beforeEach(() => {
+    authService = new AuthService();
+    localStorage.removeItem("user");
+  });
+  it("should be created", () => {
+    expect(authService).toBeTruthy();
+  });
+  it("return true when user is logged In", () => {
+    localStorage.setItem("user", "aymen");
+    expect(authService.isAuthentified()).toBeTrue();
+  });
+  it("return false when user is logged Out", () => {
+    expect(authService.isAuthentified()).toBeFalse();
   });
 });
--- src/app/unitTest/color-ut/color-ut.component.spec.ts
+++ src/app/unitTest/color-ut/color-ut.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ColorUtComponent } from "./color-ut.component";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe("ColorUtComponent", () => {
+fdescribe("ColorUtComponent", () => {
   let component: ColorUtComponent;
   let fixture: ComponentFixture<ColorUtComponent>;
+  let el: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ColorUtComponent],
@@ -12,10 +15,32 @@ describe("ColorUtComponent", () => {
 
     fixture = TestBed.createComponent(ColorUtComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+  it("should have p with initial color red", () => {
+    /*
+      Select p
+      vérify backgroundColor
+    */
+    const paragraph = el.query(By.css(`[data-testid=para]`));
+    expect(paragraph.nativeElement.style["background-color"]).toBe("red");
+  });
+
+  it("should have p with color yellow after click", () => {
+    /*
+      Select p
+      click
+      trigger changeDetection
+      vérify backgroundColor
+    */
+    const paragraph = el.query(By.css(`[data-testid=para]`));
+    paragraph.nativeElement.click();
+    fixture.detectChanges();
+    expect(paragraph.nativeElement.style["background-color"]).toBe("yellow");
   });
 });
